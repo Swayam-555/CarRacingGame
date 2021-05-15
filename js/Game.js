ec46c367bec81ebd40d1859ebe5399a5c0b7100f
Swayam-555 <radheshyam.panigrahi@gmail.com>
2021-05-15 21:13:56 +0530

    game function created

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,42 @@ class Game {
     });
   }
 
-  start(){
+   async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountRef = await database.ref('playerCount').once("value")
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+
+      }
       form = new Form()
       form.display();
     }
   }
+
+  play(){
+    form.hide();
+    textSize(35)
+    text("Game Start",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers !== undefined){
+      var H = 130;
+      for(var plr in allPlayers){
+        H+=35
+        if(plr === "player" + player.index){
+          fill("cyan") 
+        }else{
+          fill("lime")
+        }
+        textSize(20)
+        text(allPlayers[plr].name + ":" + allPlayers[plr].distance,120,H);
+      }
+      if(keyDown("UP_ARROW") && player.index !== null){
+        player.distance += 50;
+        player.update();
+      }
+    }
+  }
+
 }
